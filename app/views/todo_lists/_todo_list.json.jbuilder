96973b00dffd43c5f14eda7518d56f3f70678935
A <kassuruv2@gmail.com>
2020-08-16 12:02:05 +0900

    update layout

--- app/views/todo_lists/_todo_list.json.jbuilder
+++ app/views/todo_lists/_todo_list.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! todo_list, :id, :title, :body, :created_at, :updated_at
+json.extract! todo_list, :id, :title, :content, :limit, :created_at, :updated_at
 json.url todo_list_url(todo_list, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_15_014804) do
+ActiveRecord::Schema.define(version: 2020_08_16_023108) do
 
   create_table "todo_lists", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "limit"
+    t.integer "UserID"
   end
 
 end
